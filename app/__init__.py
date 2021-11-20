eecabac55c9f820eeae4dff5f1065aa3be04b05d
bachdv <bachdv.hust@gmail.com>
2021-11-20 23:37:15 +0700

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,6 @@ from app.api import bp as api_bp
 app.register_blueprint(api_bp, url_prefix='/api')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from app import  models
+# from app import  models
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
--- app/models.py
+++ app/models.py
@@ -1,10 +1,7 @@
 from app import db
 
-class User(db.Model):
+class PetModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(64), index=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
